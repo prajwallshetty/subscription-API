21fa80450912047d40770d2be1bb6222e2761a66
prajwallshetty <prajwal0shetty11@gmail.com>
2026-01-15 09:53:01 +0530

    subscription route

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import {PORT} from './config/env.js';
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subcription.routes.js';
+
 const app = express();
 
-import {PORT} from './config/env.js';
+app.use('api/v1/auth', authRouter);
+app.use('api/v1/users', userRouter);
+app.use('api/v1/subscriptions', subscriptionRouter);
+
 app.get('/', (req, res) => {
     res.send('welcome to the API');
 });
